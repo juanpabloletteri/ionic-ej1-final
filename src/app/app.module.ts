e3898ae664c07cf7ce3c24eafdbefff63c7861e1
juanpabloletteri <pabloletteri@gmail.com>
2017-10-02 12:10:48 -0300

    descarga de modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,52 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AboutPage } from '../pages/about/about';
+import { AplicacionPage } from '../pages/aplicacion/aplicacion';
+
+import { NgxQRCodeModule } from 'ngx-qrcode2';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyD4-Fc5pZCTLdgqewFwZdYEC66ClQS4S3E",
+  authDomain: "ej01-399f7.firebaseapp.com",
+  databaseURL: "https://ej01-399f7.firebaseio.com",
+  projectId: "ej01-399f7",
+  storageBucket: "ej01-399f7.appspot.com",
+  messagingSenderId: "489728820606"
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AboutPage,
+    AplicacionPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    NgxQRCodeModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AboutPage,
+    AplicacionPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    AngularFireDatabase,
+    BarcodeScanner,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
